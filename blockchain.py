75f2717db44f7244d786cd50c89b982f56da298e
Naman Sharma <naman99sha@gmail.com>
2024-08-26 18:59:38 +0530

    Initialised blockchain class

--- blockchain.py
+++ blockchain.py
@@ -1,1 +1,15 @@
 # Module 1 - Creating a blockchain
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+#Building blockchain
+
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, prev_hash = '0')
+
+#Mining Blockchain
