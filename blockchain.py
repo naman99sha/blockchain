2aff73e44bb9bb7cce8e84a34f49c0138a42b0a6
Naman Sharma <naman99sha@gmail.com>
2024-08-26 19:06:10 +0530

    Created create_block function

--- blockchain.py
+++ blockchain.py
@@ -11,5 +11,15 @@ class Blockchain:
     def __init__(self):
         self.chain = []
         self.create_block(proof = 1, prev_hash = '0')
+        
+    def create_block(self, proof, prev_hash):
+        block = {
+            'index': len(self.chain)+1, 
+            'timestamp': str(datetime.datetime().now()), 
+            'proof':proof, 
+            'previous_hash':prev_hash
+            }
+        self.chain.append(block)
+        return block
 
 #Mining Blockchain
